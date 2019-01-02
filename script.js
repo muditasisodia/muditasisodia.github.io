a6382dcb049d5541dfa348d7bafb610920137c97
muditasisodia <muditasisodia@gmail.com>
2019-01-02 21:38:37 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function run() {
+  if(window.innerHeight> document.querySelector(".header-text").style.height){
+    //document.querySelector(".header-text").style.height = window.innerHeight+"px";
+    console.log("chnage height");
+    console.log(window.innerHeight);
+    console.log(document.querySelector(".header-text").style.height);
+  }
+  else {
+    console.log("dnt chnage");
+  }
+}
+
+// in case the document is already rendered
+if (document.readyState!='loading') run();
+// modern browsers
+else if (document.addEventListener) document.addEventListener('DOMContentLoaded', run);
+// IE <= 8
+else document.attachEvent('onreadystatechange', function(){
+    if (document.readyState=='complete') run();
+});
